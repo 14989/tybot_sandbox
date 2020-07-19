8630a0fe964da5b9edeb7809e7bdb50389ea2556
Ed Burton <Ed.Burton@Tylertech.com>
2020-07-19 09:08:39 -0500

    removed tyvenv from repo

--- tyvenv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\erbur\desktop\tybot_sandbox\tybot_sandbox\tyvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
