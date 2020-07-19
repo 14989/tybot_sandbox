7d773f9b9c26a4c789d9c48954c005bc8e39c453
Ed Burton <Ed.Burton@Tylertech.com>
2020-07-19 16:43:03 -0500

    revised settings.py fotenv import

--- tybot/wsgi.py
+++ tybot/wsgi.py
@@ -1,17 +1,17 @@
 """
-WSGI config for tybot project.
+WSGI config for mysite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
 from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tybot.settings')
-project_folder = os.path.expanduser('~/tybot_sandbox') # adjust as appropriate
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+project_folder = os.path.expanduser('~/TyBot')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
 application = get_wsgi_application()
