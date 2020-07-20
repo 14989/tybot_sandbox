1214e13471148643b78b0eb59adf4ec157b5c34f
14989 <67342789+14989@users.noreply.github.com>
2020-07-20 11:33:59 -0500

    Update wsgi.py
    So that it works on pythonanywhere

--- tybot/wsgi.py
+++ tybot/wsgi.py
@@ -12,6 +12,6 @@ from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
-project_folder = os.path.expanduser('~/tybot_sandbox')  # adjust as appropriate
+project_folder = os.path.expanduser('~/tybot')  # adjust as appropriate
 load_dotenv(os.path.join(project_folder, '.env'))
 application = get_wsgi_application()
